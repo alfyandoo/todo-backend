f965e408593fa2b091cbdcb0c3487ad2f4216e9d
alfyandoo <alfyando2001@gmail.com>
2022-03-15 22:40:30 +0700

    Get /

--- script.ts
+++ script.ts
@@ -1,11 +1,16 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, todos } from '@prisma/client'
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient() // Query
 const express = require("express");
 const app = express();
 const port = process.env.PORT || "8000";
 
 app.use(express.json());
+
+app.get("/", async (req: any, res: any) => {
+  const todos = await prisma.todos.findMany();
+  res.json(todos);
+});
 
 app.listen(port, () => {
   console.log(`Server Running at ${port} 🚀`);
